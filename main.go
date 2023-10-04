1d4cf87450cc64de19776f250f15d9f7521a69e3
jaytrairat <jaytrairat@outlook.com>
2023-10-04 11:52:38 +0700

    feat: search ws and wss inside file

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
+	searchStrs := []string{"ws://", "wss://"}
 	filepath.Walk("./CodeExtract", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Println("Folder not found")
 		} else {
-			if !info.IsDir() && filepath.Ext(path) == ".java" {
-				fmt.Println(path)
+			if !info.IsDir() && (filepath.Ext(path) == ".java" || filepath.Ext(path) == ".kt" || filepath.Ext(path) == ".xml" || filepath.Ext(path) == ".html") {
+				file, _ := os.Open(path)
+				scanner := bufio.NewScanner(file)
+
+				for scanner.Scan() {
+					line := scanner.Text()
+					for _, searchStr := range searchStrs {
+						if strings.Contains(line, searchStr) {
+							fmt.Printf("%s\n", strings.TrimSpace(line))
+						}
+					}
+				}
+
 			}
 		}
 		return nil
